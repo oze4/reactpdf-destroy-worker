6ad89ac456737c454f4f1ce6067262cd3f89ed32
oze4 <matthewpoestreich@gmail.com>
2019-09-19 12:49:20 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ class App extends React.Component {
             }
           </ul>
         </div>
-        {this.state.show ? <MyPDF /> : ""}
+        {show ? <MyPDF /> : ""}
       </div>
     );
   }
